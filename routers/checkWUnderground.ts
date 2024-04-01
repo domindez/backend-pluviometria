69e66e2fdde52d24227115d9f5e5887806d6bc87
domindez <109239601+domindez@users.noreply.github.com>
2024-04-01 14:50:08 +0200

    save in db

--- routers/checkWUnderground.ts
+++ routers/checkWUnderground.ts
@@ -1,37 +1,42 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-// Importa las dependencias necesarias
 import express = require('express')
 import puppeteer from 'puppeteer'
+import PluvioData from '../models/pluvioData'
 
 const routerCheckWUnderground = express.Router()
 
 routerCheckWUnderground.get('/', async (req, res) => {
   try {
-    // Lanza Puppeteer
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-
-    // Navega a la página web
     await page.goto('https://www.wunderground.com/dashboard/pws/ICASTI60', {
-      waitUntil: 'networkidle2' // Espera a que la red esté inactiva (sin peticiones durante 500 ms).
+      waitUntil: 'networkidle2'
     })
-
-    // Espera a que el selector del valor de precipitación acumulada esté presente
     await page.waitForSelector('.weather__data .weather__text .wu-unit-rain .wu-value', { timeout: 10000 })
 
-    // Extrae el texto del elemento que contiene el valor de precipitación acumulada
     const precipAccumValue = await page.evaluate(() => {
       const precipAccumSpan = document.querySelector('.weather__data .weather__text .wu-unit-rain .wu-value')
-      // Utiliza el operador de encadenamiento opcional con un fallback a una cadena vacía.
       return precipAccumSpan?.textContent?.trim() ?? null
     })
 
     console.log('Valor de precipitación acumulada:', precipAccumValue)
-    await browser.close() // Cierra el navegador
+    await browser.close()
 
-    const depuredValue = precipAccumValue ?? 0 * 2.54
-    // Envía el valor extraído como respuesta
-    res.send({ value: depuredValue })
+    const depuredValue = parseFloat((parseFloat(precipAccumValue ?? '0') * 2.54).toFixed(1))
+
+    if (depuredValue > 0) {
+      const today = new Date().toISOString().split('T')[0]
+      const filter = { fecha: new Date(today) }
+      const update = { $set: { litros: depuredValue } }
+      const options = { upsert: true, new: true }
+
+      const updatedDocument = await PluvioData.findOneAndUpdate(filter, update, options)
+      console.log('Documento actualizado:', updatedDocument)
+      res.sendStatus(200)
+    } else {
+      console.log('El valor de precipitación acumulada es menor o igual a 1, no se actualiza/crea en la base de datos.')
+      res.sendStatus(200)
+    }
   } catch (error) {
     console.error('Error al realizar la petición:', error)
     res.status(500).send('Error interno del servidor')
