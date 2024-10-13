c92b63fc3e3d2ddce8e51046a878aa709eaf2540
domindez <109239601+domindez@users.noreply.github.com>
2024-10-13 19:32:21 +0200

    fix date

--- routers/insertData.ts
+++ routers/insertData.ts
@@ -8,18 +8,29 @@ dotenv.config()
 
 const routerInsertData = express.Router()
 
-// Endpoint para insertar o actualizar datos
 routerInsertData.post('/', async (req, res) => {
   const { fecha, litros, clave } = req.body
+
   // Verificar la clave
   if (clave !== process.env.CLAVE) {
     return res.status(403).send({ error: 'Clave incorrecta' })
   }
 
   try {
+    // Convertir la fecha a UTC y establecer la hora a medianoche
+    const fechaUTC = new Date(fecha)
+    fechaUTC.setUTCHours(0, 0, 0, 0)
+
     // Intentar encontrar un documento con la misma fecha
-    const existingRecord = await PluvioData.findOne({ fecha })
-    console.log(existingRecord)
+    const existingRecord = await PluvioData.findOne({
+      fecha: {
+        $gte: fechaUTC,
+        $lt: new Date(fechaUTC.getTime() + 24 * 60 * 60 * 1000)
+      }
+    })
+
+    console.log('Registro existente:', existingRecord)
+
     if (existingRecord) {
       // Si el documento existe, actualizar el valor de litros
       existingRecord.litros = litros
@@ -27,12 +38,12 @@ routerInsertData.post('/', async (req, res) => {
       res.send({ message: 'Datos actualizados correctamente', data: existingRecord })
     } else {
       // Si el documento no existe, crear uno nuevo
-      const newRecord = new PluvioData({ fecha, litros })
+      const newRecord = new PluvioData({ fecha: fechaUTC, litros })
       await newRecord.save()
       res.send({ message: 'Datos insertados correctamente', data: newRecord })
     }
   } catch (error) {
-    console.log(error)
+    console.log('Error en el backend:', error)
     res.status(500).send({ error: 'An error occurred' })
   }
 })
